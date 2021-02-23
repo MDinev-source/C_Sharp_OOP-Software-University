73bd04fba55c40e029b52bc81a0226bd72d89ec8
MDinev-source <milko01@abv.bg>
2021-02-23 20:14:08 +0200

    Completed task

---
+++ EasterRaces-OOP Project/Exam-Skeleton/EasterRaces/Models/Cars/Entities/Car.cs
@@ -0,0 +1,63 @@
+﻿using EasterRaces.Models.Cars.Contracts;
+using EasterRaces.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasterRaces.Models.Cars
+{
+    public abstract class Car : ICar
+    {
+        private string model;
+        private int horsePower;
+        private int minHorsePower;
+        private int maxHorsePower;
+
+        protected Car( string model, int horsePower, double cubicCentimeters, int minHorsePower, int maxHorsePower)
+        {
+            this.minHorsePower = minHorsePower;
+            this.maxHorsePower = maxHorsePower;
+            Model = model;
+            HorsePower = horsePower;
+            CubicCentimeters = cubicCentimeters;
+        }
+
+        public string Model
+        {
+            get
+            {
+                return this.model;
+            }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length < 4)
+                {
+                    throw new ArgumentException(string.Format (ExceptionMessages.InvalidModel, value,4));
+                }
+
+                this.model = value;
+            }
+        }
+        public int HorsePower 
+        {
+            get
+            {
+                return this.horsePower;
+            } 
+            private set
+            {
+                if (!(value >= minHorsePower && value <= maxHorsePower))
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidHorsePower, value));
+                }
+                this.horsePower = value;
+            }
+        }
+        public double CubicCentimeters { get; }
+
+        public virtual double CalculateRacePoints(int laps)
+        {
+            return this.CubicCentimeters / this.HorsePower * laps;
+        }
+    }
+}
