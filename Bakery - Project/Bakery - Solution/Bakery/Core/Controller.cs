1127525bc7d525773c63940e580400356b935c3e
MDinev-source <64278923+MDinev-source@users.noreply.github.com>
2021-04-11 08:07:43 +0300

    Starting Implement Controller Class

--- Bakery - Project/Bakery - Solution/Bakery/Core/Controller.cs
+++ Bakery - Project/Bakery - Solution/Bakery/Core/Controller.cs
@@ -1,10 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Bakery.Core
+﻿namespace Bakery.Core
 {
-    class Controller
+    using Bakery.Core.Contracts;
+    public class Controller : IController
     {
+        public string AddDrink(string type, string name, int portion, string brand)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string AddFood(string type, string name, decimal price)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string AddTable(string type, int tableNumber, int capacity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string GetFreeTablesInfo()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string GetTotalIncome()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string LeaveTable(int tableNumber)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string OrderDrink(int tableNumber, string drinkName, string drinkBrand)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string OrderFood(int tableNumber, string foodName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string ReserveTable(int numberOfPeople)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
