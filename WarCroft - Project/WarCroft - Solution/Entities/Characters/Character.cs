c910e1ea781a182f1541e6cc8c8e41ab0d503b14
MDinev-source <64278923+MDinev-source@users.noreply.github.com>
2021-04-13 19:56:30 +0300

    Create Part of Classes

---
+++ WarCroft - Project/WarCroft - Solution/Entities/Characters/Character.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using WarCroft.Constants;
+
+namespace WarCroft.Entities.Characters.Contracts
+{
+    public abstract class Character
+    {
+        private double health;
+
+        public double BaseHealth { get; set; }
+
+        public double Health
+        {
+            get
+            {
+                return this.health;
+            }
+            set
+            {
+                if (value > this.BaseHealth)
+                {
+                    value = this.BaseHealth;
+                }
+                if (value < 0)
+                {
+                    value = 0;
+                }
+                this.health = value;
+            }
+        }
+
+        public bool IsAlive { get; set; } = true;
+
+        protected void EnsureAlive()
+        {
+            if (!this.IsAlive)
+            {
+                throw new InvalidOperationException(ExceptionMessages.AffectedCharacterDead);
+            }
+        }
+    }
+}
